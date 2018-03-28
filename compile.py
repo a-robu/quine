6920fd0a62ddf33163c1f032fce22bd882322d14
Andrei Robu <andrei.rocks.1992@gmail.com>
2018-03-28 11:08:25 +0100

    add subjective simplicity

--- compile.py
+++ compile.py
@@ -1,14 +1,12 @@
 import sys
 
-fn = 'quine.py'
-
-if len(sys.argv) >= 2:
-    fn = sys.argv[1]
+fn = sys.argv[1]
 
 with open(fn) as f:
     raw_contents = f.read()
     left, raw_right = raw_contents.split('<<TAPE>>')
+    # `at` is the index where the tape begins.
     at = len(left)
     right = raw_right.replace('<<AT>>', str(at))
-    middle = str(list(map(ord, left + right)))
+    middle = str([ord(x) for x in left + right])
     print(left + middle + right)
