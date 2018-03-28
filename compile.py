e97a0962bbac10ae7e78536ef82d5b76af08adc5
Andrei D. Robu <andrei.rocks.1992@gmail.com>
2018-03-28 14:34:22 +0100

    dedented code out of the block

--- compile.py
+++ compile.py
@@ -4,9 +4,10 @@ fn = sys.argv[1]
 
 with open(fn) as f:
     raw_contents = f.read()
-    left, raw_right = raw_contents.split('<<TAPE>>')
-    # `at` is the index where the tape begins.
-    at = len(left)
-    right = raw_right.replace('<<AT>>', str(at))
-    middle = str([ord(x) for x in left + right])
-    print(left + middle + right)
+
+left, raw_right = raw_contents.split('<<TAPE>>')
+# `at` is the index where the tape begins.
+at = len(left)
+right = raw_right.replace('<<AT>>', str(at))
+middle = str([ord(x) for x in left + right])
+print(left + middle + right)
